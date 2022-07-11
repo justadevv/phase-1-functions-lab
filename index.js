5fe9b1ea90cd4a36448aff38916138c9eaecbb23
billy baney <bbaneydev@gmail.com>
2022-07-10 20:53:19 -0500

    finished project

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(a) {
-    let hq = 50
+    let hq = 42
     if (a > hq) {
         return a - hq
-    } else {
+    } else if (a < hq) {
         return hq - a
-    }
-}
-console.log(distanceFromHqInBlocks(52))
-
-
-function distanceFromHqInFeet(a) {
-    return distanceFromHqInBlocks(a) * 264
-}
-console.log(distanceFromHqInFeet(52))
-
-
-function distanceTraveledInFeet(start, destination) {
-    if (start > destination) {
-        return (start - destination) * 264
     } else {
-        return (destination - start) * 264
+        return 'Please enter a starting block'
     }
 }
-console.log(distanceTraveledInFeet(45, 50))
+
+distanceFromHqInBlocks(50)
 
 
-function calculatesFarePrice(start, destination) {
-    let distance = distanceTraveledInFeet(start, destination)
+function distanceFromHqInFeet(b) {
+    return distanceFromHqInBlocks(b) * 264
+}
 
-    if (distance <= 400) {
-        return 'free'
-    } else if (distance > 400 && distance <= 2000) {
-        return .02 * (distance - 400)
-    } else if (distance > 2000 && distance <= 2500) {
-        return 25;
+distanceFromHqInFeet(41)
+
+
+function distanceTravelledInFeet(a, b) {
+    if (a > b) {
+        return (a - b) * 264
     } else {
-        return 'Cannot travel that far'
+        return (b - a) * 264
     }
 }
 
-console.log(calculatesFarePrice(30, 50))
+distanceTravelledInFeet(62, 60)
+
+function calculatesFarePrice(a, b) {
+    let distance = distanceTravelledInFeet(a, b)
+    if (distance < 400) {
+        return "It's free!"
+    } else if (distance > 400 && distance < 2000) {
+        return (distance - 400) * 0.02
+    } else if (distance > 2000 && distance < 2500) {
+        return "It's twenty-five dollars."
+    } else if (distance > 2500) {
+        return "We cannot travel that far"
+    }
+}
+
+console.log(calculatesFarePrice(34, 24))
